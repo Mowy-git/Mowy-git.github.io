47ac971c17309b81ad90661e8e0cfc69155ed163
Mowy-git <mowyml@gmail.com>
2025-03-12 14:26:45 +0800

    hello

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var rps = ["rock", "paper", "scissors"];
 let humanScore = 0;
 let computerScore = 0;
-let draw = 0;
 
 function getComputerChoice() {
     return rps[Math.floor(Math.random() * rps.length)];
@@ -12,42 +11,47 @@ function getHumanChoice() {
 }
 
 function playRound(getHumanChoice, getComputerChoice) {
+    
+
+    const h = document.querySelector("#humanscore");
+    const c = document.querySelector("#computerscore");
+    const result = document.querySelector("#result");
+
+    if (humanScore >= 5) {
+        result.innerHTML = "Human wins";
+        humanScore = 0;
+        computerScore = 0;
+    } else if (computerScore >= 5) {
+        result.innerHTML = "Computer wins";
+        humanScore = 0;
+        computerScore = 0;
+    }
+
+    h.innerHTML = `Human score: ${humanScore}`;
+    c.innerHTML = `Computer score: ${computerScore}`;
     if (!rps.includes(getHumanChoice)) {
-        console.log("Invalid");
+        result.innerHTML = "Invalid";
     } else if (getHumanChoice == "rock" && getComputerChoice == "scissors" || getHumanChoice == "paper" && getComputerChoice == "rock" || getHumanChoice == "scissors" && getComputerChoice == "paper") {
         humanScore++;
-    } else if (getHumanChoice == getComputerChoice) {
-        draw++;
+    } else if (getComputerChoice == getHumanChoice) {
+        result.innerHTML = "Draw";
     } else {
         computerScore++;
     }
 }
 
-function playGame() {
-    
-    for (let i=0;i<5;i++) {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        console.log(humanSelection);
-        console.log(computerSelection);
-        console.log("\n");
-        playRound(humanSelection, computerSelection);
-    }
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
-    console.log(`Score: \nHuman: ${humanScore} \nComputer: ${computerScore}`);
+rock.addEventListener("click" , () => {
+    playRound("rock", getComputerChoice());
+});
 
-    if (draw > 0)
-        console.log(`Draw: ${draw}`)
+paper.addEventListener("click" , () => {
+    playRound("paper", getComputerChoice());
+});
 
-    if (humanScore > computerScore)
-        console.log("Human wins");
-    else if (humanScore < computerScore)
-        console.log("Computer wins");
-    else
-        console.log("Draw");
-}
-
-
-playGame();
-
-
+scissors.addEventListener("click" , () => {
+    playRound("scissors", getComputerChoice());
+});
